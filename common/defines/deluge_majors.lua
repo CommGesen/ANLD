e7efa6ba0b1dcae900603029207d87a0ee13b543
Szymon3000 <99537135+Szymon3000@users.noreply.github.com>
2025-05-15 17:22:02 +0200

    15th May 2025
    -removed portraits that don't belong to us in Arab countries;
    -changed requirements to become a major - now it is 40 factories instead of 16;
    -now stability and war support can go down to -100% instead of 0%;
    -added new provinces, changed some provinces;
    -added new states;
    -added many map files;
    -removed Arab music;
    -other fixes;

--- common/defines/deluge_majors.lua
+++ common/defines/deluge_majors.lua
@@ -1,2 +1,4 @@
-NDefines.NCountry.MAJOR_MIN_FACTORIES = 16						-- need at least these many factories to become a major
-NDefines.NCountry.ADDITIONAL_MAJOR_COUNTRIES_IC_RATIO = 0.3		-- Countries will also be considered major when having more factories that the average of top MIN_MAJOR_COUNTRIES countries' factories times ADDITIONAL_MAJOR_COUNTRIES_IC_RATIO
+NDefines.NCountry.MAJOR_MIN_FACTORIES = 40						-- need at least these many factories to become a major
+NDefines.NCountry.MAJOR_IC_RATIO = 10
+NDefines.NCountry.MIN_STABILITY = -100
+NDefines.NCountry.MIN_WAR_SUPPORT = -100
